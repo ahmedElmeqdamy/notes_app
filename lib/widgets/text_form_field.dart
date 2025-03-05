e162a178cd6b75523dcdb545bd334ff32b893296
ahmed <ahmedelmeqdamy223@gmail.com>
2025-03-05 11:12:29 +0200

    add textformfield

---
+++ lib/widgets/text_form_field.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+class CustomTextFormField extends StatelessWidget {
+  final String hintText;
+  final IconData? suffixIcon;
+  final bool obscureText;
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? validator;
+  final ValueChanged<String>? onChanged;
+  final int? maxLine;
+
+
+
+
+
+   CustomTextFormField({
+    Key? key,
+    required this.hintText,
+    this.suffixIcon,
+    this.obscureText = false,
+    this.controller,
+    this.validator,
+    this.onChanged,  this.maxLine,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:  EdgeInsets.all(8.0),
+      child: TextFormField(
+        maxLines: maxLine,
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          hintText: hintText,
+          suffixIcon: suffixIcon != null ? Icon(suffixIcon) : null,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+        ),
+        validator: validator,
+        onChanged: onChanged,
+      ),
+    );
+  }
+}
