36fa05fa8c2e76991506e88a59befc81d42b3107
ahmed <ahmedelmeqdamy223@gmail.com>
2025-03-06 03:04:45 +0200

    add note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String dateTime;
+  final int color;
+
+  const NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.dateTime,
+    required this.color,
+  });
+}
